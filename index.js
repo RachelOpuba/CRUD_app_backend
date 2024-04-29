a5760361f14a588a89da51c5bbfa9ea96c822f81
Rachel Opuba <ogbenirach@gmail.com>
2024-04-29 03:03:06 +0100

    Added a  .env file

--- index.js
+++ index.js
@@ -1,83 +1,84 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000
 const app = express();
-const Product = require('./models/product.models')
+const Product = require("./models/product.models");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
+app.get("/", (req, res) => {
+  res.send("Hello from Node API again and again");
+});
 
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get('/', (req, res )=>{
-    res.send('Hello from Node API again and again')
-})
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find(res.body);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.post('/api/products', async (req, res )=>{
-    try{
-       const product =  await Product.create(req.body)
-       res.status(200).json(product)
-    }catch(error){
-        res.status(500).json({message:error.message})
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-})
 
-app.get('/api/products', async (req, res )=>{
-    try{
-       const products =  await Product.find(res.body)
-       res.status(200).json(products)
-    }catch(error){
-        res.status(500).json({message:error.message})
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
     }
-})
+    res.status(200).json({ message: "Product deleted Successfylly" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get('/api/product/:id', async (req, res )=>{
-    try{
-        const {id} = req.params;
-       const product =  await Product.findById(id)
-       res.status(200).json(product)
-    }catch(error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-
-app.put('/api/product/:id', async (req, res )=>{
-    try{
-        const {id} = req.params;
-       const product =  await Product.findByIdAndUpdate(id, req.body)
-
-       if(!product){
-        return(res.status(404).json({message: "Product not found"}))
-       }
-
-       const updatedProduct = await Product.findById(id)
-       res.status(200).json(updatedProduct)
-    }catch(error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-app.delete('/api/product/:id', async (req, res )=>{
-    try{
-        const {id} = req.params;
-     const product = await Product.findByIdAndDelete(id)
-
-       if(!product){
-        return(res.status(404).json({message: "Product not found"}))
-       }
-       res.status(200).json({message: "Product deleted Successfylly"})
-
-    }catch(error){
-        res.status(500).json({message:error.message})
-    }
-})
-
-mongoose.connect('mongodb+srv://admin:esrvV2BiZQJoNhqr@backenddb.gzwyywu.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB')
-.then(()=>{
-    console.log("Connected to the database")
-    app.listen(3001, () => {
-        console.log(`Server Started at ${3001}`)
-    })
-}).catch(()=>{
-    console.log("Connection falled")
-})
+mongoose
+  .connect(
+    "mongodb+srv://admin:esrvV2BiZQJoNhqr@backenddb.gzwyywu.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(PORT, () => {
+      console.log(`Server Started at ${PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection falled");
+  });
